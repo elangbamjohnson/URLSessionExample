d127d8797a7faee0e3404b6d5f1bae127be726ca
Johnson Elangbam <johnsonelangbam@Johnsons-MacBook-Pro.local>
2019-02-28 22:02:14 +0530

    First commit

---
+++ URLSessionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSessionExample
+//
+//  Created by Johnson Elangbam on 2/28/19.
+//  Copyright © 2019 Johnson Elangbam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
